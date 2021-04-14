6015f6170deac4740f8cc76faeb2f2d9ebf62634
KosM <kosukemorimoto1@gmail.com>
2021-04-14 14:07:24 +0900

    implemented sign up feature with firebase

---
+++ src/components/register/register.component.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+
+import './register.styles.scss';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+const Register = () => {
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Your passwords don't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, { displayName });
+
+      // Clear the form
+      setDisplayName('');
+      setEmail('');
+      setPassword('');
+      setConfirmPassword('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'displayName':
+        setDisplayName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      case 'confirmPassword':
+        setConfirmPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="register">
+      <h2 className="title">Do not have a account yet?</h2>
+      <span>Register with your email and password</span>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={handleChange}
+          label="Display Name"
+          required
+        />
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          label="Email"
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          label="Password"
+          required
+        />
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
+          label="Confirm Password"
+          required
+        />
+        <CustomButton type="submit">Register</CustomButton>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
--- src/pages/sign-in-and-register/sign-in-and-register.component.jsx
+++ src/pages/sign-in-and-register/sign-in-and-register.component.jsx
@@ -1,9 +1,11 @@
 import './sign-in-and-register.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component.jsx';
+import Register from '../../components/register/register.component';
 
 const SignInAndRegisterPage = () => (
   <div className="sign-in-and-register">
     <SignIn />
+    <Register />
   </div>
 );
 
