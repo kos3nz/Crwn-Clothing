eb70fb098fad570b1c578d918e11a599177cf981
KosM <kosukemorimoto1@gmail.com>
2021-04-15 18:59:15 +0900

    updated shop data with new IDs, created cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,38 @@
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{`$${price}`}</span>
-    </div>
-  </div>
-);
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
 
-export default CollectionItem;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{`$${price}`}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
+    </div>
+  );
+};
+
+//:: =============== Redux =============== :://
+
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+//:: =============== Export =============== :://
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
   // Since I only need to toggle the boolean value on hidden property, I only need to specify the type. I don't have to pass a payload value.
 });
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
