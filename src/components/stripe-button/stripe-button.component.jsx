1090f143379cc333fe21c878e836acb021ad5807
KosM <kosukemorimoto1@gmail.com>
2021-04-17 22:23:25 +0900

    implemented stripe payment integration and updated favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51IdZAtD24k8wOsaORDd0No2riHXFUqNwGcmwEDaqq0I5PU49KXOcTc0aTzmc1BSzKV5PG2fOguUEaohpKmkasVTe00sUsJjNT2';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total price is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
