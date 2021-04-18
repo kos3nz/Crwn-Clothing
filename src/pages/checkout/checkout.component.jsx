8765856f78379c1acc72ad8b803963f2c17b96a0
KosM <kosukemorimoto1@gmail.com>
2021-04-18 10:23:55 +0900

    fixed a bug in user.selectors.js

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -41,7 +41,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="test-warning">
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 02/22 - CVV: 222
+      4242 4242 4242 4242 - Exp: 02/22 - CVC: 222
     </div>
     <StripeCheckoutButton price={total} />
   </div>
